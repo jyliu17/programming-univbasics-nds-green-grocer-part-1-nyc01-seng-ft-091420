3e919b96972375cfd40d80fe7f0a639b5004c310
Johnson Liu <jyliu17@gmail.com>
2020-09-13 19:49:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+index = 0
+  while index < collection.length do
+    return collection[index] if name === collection[index][:item]
+    index += 1
+  end
+  nil
+end
 end
 
 def consolidate_cart(cart)
